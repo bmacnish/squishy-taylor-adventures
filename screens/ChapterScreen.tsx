247ff227db5da99e87495f97fd3ae217c5782054
Brienna Macnish <brienna.macnish@gmail.com>
2021-05-29 16:47:15 +1000

    Add audio files and basic play/pause functionality

--- screens/ChapterScreen.tsx
+++ screens/ChapterScreen.tsx
@@ -1,9 +1,21 @@
-import { useRoute } from '@react-navigation/native'
-import React from 'react'
-import { View, Text } from 'react-native'
-import { chapters, ChapterType } from '../constants/Chapters'
+import React, { useEffect, useState } from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { chapters } from '../constants/Chapters'
+import { Audio, AVPlaybackStatus } from 'expo-av'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    paddingHorizontal: 8,
+  },
+})
 
 export default function ChapterScreen({ route }) {
+  const [sound, setSound] = useState<Audio.Sound | null>()
+  const [status, setStatus] = useState<AVPlaybackStatus | null>()
+
   const { chapterId } = route.params
 
   const getChapterById = (chapterId: number) => {
@@ -12,10 +24,43 @@ export default function ChapterScreen({ route }) {
 
   const chapter = getChapterById(chapterId)
 
+  const playAudio = async () => {
+    const { sound, status } = await Audio.Sound.createAsync(
+      require(`../assets/audio/chapterAudio/1.mp3`)
+    )
+
+    setSound(sound)
+    setStatus(status)
+
+    await sound.playAsync()
+  }
+
+  const pauseAudio = async () => {
+    if (status?.isLoaded) {
+      sound?.setStatusAsync
+    }
+    await sound?.pauseAsync()
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound')
+          sound.unloadAsync()
+        }
+      : undefined
+  }, [sound])
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>{chapter.chapterNumber}</Text>
       <Text>{chapter.title}</Text>
+      <TouchableOpacity onPress={playAudio}>
+        <Text>Play Audio</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={pauseAudio}>
+        <Text>Pause Audio</Text>
+      </TouchableOpacity>
     </View>
   )
 }
