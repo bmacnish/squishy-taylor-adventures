4a8bdbbeae047ec7148749ee783cb4fe3b067d5d
Brienna Macnish <brienna.macnish@gmail.com>
2021-05-30 14:21:17 +1000

    Add styled text to app

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,7 +1,42 @@
-import * as React from 'react';
+import * as React from 'react'
+import { StyleProp, Text, TextProps, TextStyle, StyleSheet } from 'react-native'
 
-import { Text, TextProps } from './Themed';
-
-export function MonoText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />;
+interface AppTextProps {
+  children?: React.ReactNode
 }
+
+const styles = StyleSheet.create({
+  h1: {
+    fontSize: 40,
+    lineHeight: 44,
+  },
+  h2: {
+    fontSize: 34,
+    lineHeight: 38,
+  },
+  h3: {
+    fontSize: 24,
+    lineHeight: 28,
+  },
+  h4: {
+    fontSize: 18,
+    lineHeight: 22,
+  },
+  body1: {
+    fontSize: 16,
+    lineHeight: 20,
+  },
+})
+
+const createTextComponent =
+  (styles: StyleProp<TextStyle>) => (props: AppTextProps & TextProps) => {
+    const { style: textStyle, ...textProps } = props
+
+    return <Text style={[styles, textStyle]} {...textProps} />
+  }
+
+export const H1Text = createTextComponent(styles.h1)
+export const H2Text = createTextComponent(styles.h2)
+export const H3Text = createTextComponent(styles.h3)
+export const H4Text = createTextComponent(styles.h4)
+export const Body1 = createTextComponent(styles.body1)
