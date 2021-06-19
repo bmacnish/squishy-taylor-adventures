5a3190f24e81542e4bb322af78f34ecbc0a3fa01
Brienna Macnish <brienna.macnish@gmail.com>
2021-06-19 12:07:49 +1000

    Update content

--- constants/Chapters.ts
+++ constants/Chapters.ts
@@ -5,37 +5,48 @@ export interface ChapterType {
   chapterNumber: string
   title: string
   text: string
+  nextPage?: number
 }
 
 export const chapters: Array<ChapterType> = [
+  {
+    chapterId: 0,
+    chapterNumber: 'Welcome',
+    title: 'Getting started',
+    text: text.prologue,
+  },
   {
     chapterId: 1,
     chapterNumber: 'Chapter One',
     title: 'There’s a ghost mystery...',
     text: text.chapterOne,
+    nextPage: 16,
   },
   {
     chapterId: 2,
     chapterNumber: 'Chapter Two',
     title: 'The ghost map gets bigger',
-    text: `You’ve definitely proved that you’re excellent at sleuthing. I’d love for you to solve this ghost mystery with me!
-
-          You’re right, the three places the ghosts have visited near me are the playground, bus shelter and car park. These places have one thing in common. The lights are on all the time.
-
-          Do you have a place nearby to you that has lights on all the time? Can you use your Detective Drawing Skills to make a map for me? Draw your ‘lights on all the time’ place on it too. This way we can start to build a bigger ghost map.
-
-          When you’re done, send your map to me at squishysleuthing@gmail.com, that’s S Q U I S H Y S L E U T H I N G @gmail.com and I’ll reply – check your junk mail if you don’t hear from me!`,
+    text: text.chapterTwo,
   },
   {
     chapterId: 3,
     chapterNumber: 'Chapter Three',
     title: 'Venture into the night!',
-    text: `Thanks for that map. That definitely looks like a place where the lights are on day and night.I’ve given your location a code name. It’s Big Dipper—the brightest stars in the sky!
-
-    Your map made me think of a couple of things. If the ghosts appear where the lights are on, they actually want us to see them. What do they want us to see though?
-
-    I also remembered this nature strip near me that’s always lit. I’m sure the ghosts will go there tonight. If I can venture into the night and set up a camera there, I can record the ghosts and watch the video in the morning. Maybe I can see what they’re trying to show us.
-
-    Can you help me find the best time to sneak out?`,
+    text: text.chapterThree,
+    nextPage: 14,
+  },
+  {
+    chapterId: 4,
+    chapterNumber: 'Chapter Four',
+    title: 'Venture into the night!',
+    text: text.chapterThree,
+    nextPage: 24,
+  },
+  {
+    chapterId: 5,
+    chapterNumber: 'Chapter Five',
+    title: 'Venture into the night!',
+    text: text.chapterThree,
+    nextPage: 30,
   },
 ]
